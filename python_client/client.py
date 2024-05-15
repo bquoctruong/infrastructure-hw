50613e966f85743a254b779d1c98d280995c070c
bquochtruong <brianquochungtruong@gmail.com>
2024-05-14 19:36:37 -0700

    Init commit for deployment YAML; updated syntax

--- python_client/client.py
+++ python_client/client.py
@@ -4,8 +4,8 @@ import websockets
 
 # Function client that sends message "Hello, Server!" to server listed in URI
 async def client():
-    # Sub var ws values for docker: host.docker.internal, k8s: websocket-server-service
-    uri = "ws://websocket-server-service:65432"
+    # Sub var ws values for docker: host.docker.internal, k8s: python-server-service
+    uri = "ws://python-server-service:65432"
     async with websockets.connect(uri) as websocket:
         while True:
             # Send server message
